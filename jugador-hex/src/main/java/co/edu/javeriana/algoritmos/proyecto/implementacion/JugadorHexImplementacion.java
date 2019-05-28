031c78733f3c78754052682792d0aea3efd0bd2f
maestrooso <oscarjcr95@gmail.com>
2019-05-27 20:56:34 -0500

    Added Implementation Folder with classes to implement designed methods

---
+++ jugador-hex/src/main/java/co/edu/javeriana/algoritmos/proyecto/implementacion/JugadorHexImplementacion.java
@@ -0,0 +1,22 @@
+package co.edu.javeriana.algoritmos.proyecto.implementacion;
+
+import co.edu.javeriana.algoritmos.proyecto.ColorJugador;
+import co.edu.javeriana.algoritmos.proyecto.Jugada;
+import co.edu.javeriana.algoritmos.proyecto.JugadorHex;
+import co.edu.javeriana.algoritmos.proyecto.Tablero;
+
+public class JugadorHexImplementacion implements JugadorHex{
+
+	@Override
+	public Jugada jugar(Tablero tablero, ColorJugador color) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String nombreJugador() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ jugador-hex/src/main/java/co/edu/javeriana/algoritmos/proyecto/implementacion/TableroImplementacion.java
@@ -0,0 +1,27 @@
+package co.edu.javeriana.algoritmos.proyecto.implementacion;
+
+import co.edu.javeriana.algoritmos.proyecto.ColorJugador;
+import co.edu.javeriana.algoritmos.proyecto.Jugada;
+import co.edu.javeriana.algoritmos.proyecto.Tablero;
+
+public class TableroImplementacion implements Tablero{
+
+	@Override
+	public void aplicarJugada(Jugada jugada, ColorJugador colorJugador) {
+		// TODO Auto-generated method stub
+		return;
+	}
+
+	@Override
+	public ColorJugador ganador() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ColorJugador casilla(int fila, int columna) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
